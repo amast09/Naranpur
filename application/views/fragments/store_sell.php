92ed5894f0796dd2d1bad414b459abce2d81b51f
Aaron Mast <amast09@gmail.com>
2014-02-27 21:41:16 -0500

    Completed Store Javascript refactoring.

--- application/views/fragments/store_sell.php
+++ application/views/fragments/store_sell.php
@@ -1,7 +1,7 @@
-<form class="horizontal" action="<?php echo site_url();?>/store/sell" method="POST"> 
+<form id="sell" class="horizontal" action="<?php echo site_url();?>/store/sell" method="POST"> 
 
 	<div class="control-group">
-		<select id="sellItemSelect" name="sellItem">
+		<select class="resource" name="sellItem">
 			<?php foreach($sell_inventory->result() as $sell_item){ ?> 
 				<option value="<?php echo $sell_item->id; ?>" data-quantity="<?php echo $sell_item->quantity?>" data-price="<?php echo $sell_item->sellPrice?>">
 					<?php echo $sell_item->name;?>
@@ -13,80 +13,24 @@
 	</div>
 
 	<div class="control-group">
-		<input id="sellQuantitySelectBox" name="sellQuantity" type="number" min="1" max="<?=$sell_inventory->row()->quantity;?>" step='1' value="1"/>
+		<input class="quantity" name="sellQuantity" type="number" min="1" max="<?=$sell_inventory->row()->quantity;?>" step="1" value="0"/>
 	</div>
 
 	<div class="control-group">
-		<button id="sellButton" class="btn btn-primary" type="submit">Sell</button>
+		<button class="btn btn-primary" type="submit">Sell</button>
 	</div>
 
 	<div class="control-group">
-			<input type="text" id="sellCost" value="Price: $0" disabled/>
+			<input type="text" class="cost" value="Price: $0" disabled/>
 	</div>
 
 	<div class="control-group">
-			<input type="text" id="sellAvailable" value="Updated Cash: $<?php echo $cash; ?>" disabled/>
+			<input type="text" class="newCash" value="Updated Cash: $<?php echo $cash; ?>" disabled/>
 	</div>
 
-	<div id="sell_error" class="alert alert-block alert-error" style="display:none;">  
-  	<a class="close" onclick="$('#sell_error').hide()">X</a>  
+	<div class="alert alert-block alert-error" style="display:none;">  
 		<h4 class="alert-heading">Error!</h4>  
-		<p id="sell_error_message"></p>
+		<p class="error_message"></p>
 	</div> 
 
 </form>
-
-
-<script>
-var price = <?=$sell_inventory->row()->sellPrice;?>;
-var quantity = 1;
-sellCal();
-
-$(document).ready(function() {
-	$('#sellQuantitySelectBox').val(1);
-	quantity = 1;
-	$('#sellButton').removeAttr("disabled");
-});
-
-//Set up the on-click for the buttons that select the items
-$('#sellItemSelect').change(function () {
-	$('#sellQuantitySelectBox').val(1);
-	$('#sellQuantitySelectBox').attr("max", $('option:selected', this).data('quantity'));
-		price = $('option:selected', this).data('price'); 
-		quantity = 1;
-
-		if(price == null) price = 0;
-
-		sellCal();
-});
-
-$('#sellQuantitySelectBox').change(function () {
-	quantity = $(this).val();
-
-	sellCal();
-});
-
-function sellCal(){
-	var cash = <?php echo $cash; ?>;
-	var sellAvailable = document.getElementById("sellAvailable");
-	var sellCost = document.getElementById("sellCost");
-
-	if(isNaN(quantity) == true || quantity == "" || quantity < 1){
-		$('#sellButton').attr("disabled", "disabled");
-		$('#sell_error_message').text("You must enter a valid number.");
-		$('#sell_error').show();
-	}
-	else if(quantity > $('#sellItemSelect option:selected').data('quantity')){
-		$('#sellButton').attr("disabled", "disabled");
-		$('#sell_error_message').text("You do not have sufficent inventory for this transaction.");
-		$('#sell_error').show();
-	}
-	else{
-		$('#sellButton').removeAttr("disabled");
-	}
-
-	sellCost.value = "Price: " + price * quantity;
-	sellAvailable.value = "Updated Cash: $" + (cash + (price * quantity));
-}
-
-</script>
--- public_html/resources/store_view/js/store.js
+++ public_html/resources/store_view/js/store.js
@@ -1,89 +1,112 @@
 $(function() {
 
-	var buy = function(){
-		var quantity = "#buyQuantitySelectBox";
-		var item = "#buyItemSelect";
-		var cash = "#player-cash";
+	function quantityChange(event) {
+		updateTotals(event.data.selector);
+		processTransaction(event.data);
+	}
 
-		function quantityChange(){
-			updateTotals();
-			var transaction = validPurchase();
-			if(transaction.valid){
-				$('#error').hide();
-				$('#buyButton').removeAttr("disabled");
-			} else {
-				$('#buyButton').attr("disabled", "disabled");
-				$('#error_message').text(transaction.error);
-				$('#error').show();
+	function resourceChange(event) {
+		$(event.data.selector + " .alert").hide();
+		resetTotals(event.data.selector);
+		updateTotals(event.data.selector);
+		processTransaction(event.data);
+	}
+
+	function processTransaction(action){
+		var transaction = action.checkValidity();
+		if(transaction.valid){
+			$(action.selector + " .alert").hide();
+			$(action.selector + " .btn").removeAttr("disabled");
+		} else {
+			$(action.selector + " .btn").attr("disabled", "disabled");
+			$(action.selector + " .error_message").text(transaction.error);
+			$(action.selector + " .alert").show();
+		}
+	}
+
+	function updateTotals(action) {
+		var currentQuantity = parseFloat($(action + " .quantity").val()),
+			currentPrice = parseFloat($(action + ' .resource option:selected').attr("data-price")),
+			currentCash = parseFloat($("#player-cash").attr("data-player-cash")),
+			costOfTransaction = (currentQuantity * currentPrice);
+
+			if(!isNaN(costOfTransaction) && !isNaN(currentCash)){
+				$(action + " .cost").val("Price: $" + costOfTransaction);
+				if(action === "#buy"){
+					$(action + " .newCash").val("Updated Cash: $" + (currentCash - costOfTransaction));
+				}
+				else if(action === "#sell"){
+					$(action + " .newCash").val("Updated Cash: $" + (currentCash + costOfTransaction));
+				}
 			}
+	}
+
+	function resetTotals(action) {
+		$(action + " .quantity").val(1).attr("max", $('option:selected', this).data('quantity'));
+	}
+
+	function transactionSubmit(event) {
+		return(event.data.checkValidity().valid);
+	}
+
+	function validPurchase() {
+		var currentQuantity = parseFloat($("#buy .quantity").val()),
+			$resourceSelected = $('#buy .resource option:selected'),
+			currentPrice = parseFloat($resourceSelected.attr("data-price")),
+			currentAvailable = parseFloat($resourceSelected.attr("data-quantity")),
+			currentCash = parseFloat($("#player-cash").attr("data-player-cash")),
+			validity = true,
+			errorMessage = "";
+
+		if(isNaN(currentQuantity) === true || currentQuantity === "" || currentQuantity < 1){
+			errorMessage = "Please enter a valid number.";
+			validity = false;
+		}
+		else if(currentQuantity > currentAvailable){
+			errorMessage = "The store does not have the Inventory to support this transaction.";
+			validity = false;
+		}
+		else if(currentCash - (currentPrice * currentQuantity) < 0){
+			errorMessage = "You do not have the funds to buy this.";
+			validity = false;
 		}
 
-		function resourceChange(){
-			$('#error').hide();
-			resetTotals();
-			var transaction = validPurchase();
-			if(transaction.valid){
-				$('#error').hide();
-				$('#buyButton').removeAttr("disabled");
-			} else {
-				$('#buyButton').attr("disabled", "disabled");
-				$('#error_message').text(transaction.error);
-				$('#error').show();
-			}
+
+		return({"valid" : validity, "error" : errorMessage});
+	}
+
+	function validSale () {
+		var currentQuantity = $('#sell .quantity').val(),
+			validity = true,
+			errorMessage = "";
+
+		if(isNaN(currentQuantity) === true || currentQuantity === "" || currentQuantity < 1){
+			validity = false;
+			errorMessage = "You must enter a valid number.";
+		}
+		else if(currentQuantity > $('#sell .resource option:selected').data('quantity')){
+			validity = false;
+			errorMessage = "You do not have sufficent inventory for this transaction.";
 		}
 
-		function purchaseSubmit(){
-			return(validPurchase().valid);
-		}
+		return({"valid" : validity, "error" : errorMessage});
+	}
 
-		function validPurchase(){
-			var currentQuantity = parseFloat($(quantity).val()),
-				currentPrice = parseFloat($(item + ' option:selected').attr("data-price")),
-				currentAvailable = parseFloat($(item + ' option:selected').attr("data-quantity")),
-				currentCash = parseFloat($(cash).attr("data-player-cash")),
-				validity = true,
-				errorMessage = "";
-
-			if(isNaN(currentQuantity) === true || currentQuantity === "" || currentQuantity < 1){
-				errorMessage = "Please enter a valid number.";
-				validity = false;
-			}
-			else if(currentQuantity > currentAvailable){
-				errorMessage = "The store does not have the Inventory to support this transaction.";
-				validity = false;
-			}
-			else if(currentCash - (currentPrice * currentQuantity) < 0){
-				errorMessage = "You do not have the funds to buy this.";
-				validity = false;
-			}
-
-
-			return({"valid" : validity, "error" : errorMessage});
-		}
-
-		function updateTotals(){
-			var currentQuantity = parseFloat($(quantity).val()),
-				currentPrice = parseFloat($(item + ' option:selected').attr("data-price")),
-				currentCash = parseFloat($(cash).attr("data-player-cash")),
-				costOfTransaction = (currentQuantity * currentPrice);
-
-			$("#buyCost").val("Price: " + costOfTransaction);
-			$("#buyAvailable").val("Updated Cash: " + (currentCash - costOfTransaction));
-		}
-
-		function resetTotals(){
-			$(quantity).val(1);
-			$(quantity).attr("max", $('option:selected', this).data('quantity'));
-		}
-
-		return({
-			"quantityChange" : quantityChange,
-			"resourceChange" : resourceChange,
-			"purchaseSubmit" : purchaseSubmit
-		});
+	var buyAction = {
+		"selector" : "#buy",
+		"checkValidity" : validPurchase
 	};
 
-	$("#buyQuantitySelectBox").on("change", buy().quantityChange);
-	$("#buyItemSelect").on("change", buy().resourceChange);
-	$("#buy_form").on("submit", buy().purchaseSubmit);
+	var sellAction = {
+		"selector" : "#sell",
+		"checkValidity" : validSale
+	};
+
+	$("#buy .quantity").on("change", buyAction, quantityChange);
+	$("#buy .resource").on("change", buyAction, resourceChange);
+	$("#buy").on("submit", buyAction, transactionSubmit);
+
+	$("#sell .quantity").on("change", sellAction, quantityChange);
+	$("#sell .resource").on("change", sellAction, resourceChange);
+	$("#sell").on("submit", sellAction, transactionSubmit);
 });
