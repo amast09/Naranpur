47e68c41cabe6d9e2bc5014c5fd819a90d441e40
Aaron Mast <amast09@gmail.com>
2014-03-05 20:25:08 -0500

    Refactored create family JS code.

--- public_html/resources/create_family_view/js/createFamily.js
+++ public_html/resources/create_family_view/js/createFamily.js
@@ -1,12 +1,12 @@
 $(function() {
 
-$('input').change( function() { $('#error').hide();  });
-
+	$('input').change( function() { $('#error').hide();  });
 
 	$("button").on("click", function(event){
 		event.preventDefault();
-		var data = $("#new-family").serialize();
-		var submitUrl = $("#new-family").attr("data-url");
+		var data = $("#new-family").serialize(),
+				submitUrl = $("#new-family").attr("data-url"),
+				redirectUrl = $("#new-family").attr("data-redirect-url");
 
 		$.ajax({
 			type: "POST",
@@ -14,7 +14,7 @@ $('input').change( function() { $('#error').hide();  });
 			data: data,
 			dataType: "json",
 			success: function(data){
-				if(data.success) window.location.assign('<?=site_url("dashboard");?>');
+				if(data.success) window.location.assign(redirectUrl);
 				else{
 					$('#error').slideDown();
 					$('#error-message').html(data.message);
@@ -22,4 +22,5 @@ $('input').change( function() { $('#error').hide();  });
 			}
 		});
 	});
+
 });
