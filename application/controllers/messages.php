67e6b2eb13615e6aac76ccc340443c062a1cda67
Aaron Mast <amast09@gmail.com>
2014-03-16 15:39:18 -0400

    Testing the new branch.

--- application/controllers/messages.php
+++ application/controllers/messages.php
@@ -10,6 +10,7 @@ class Messages extends CI_Controller{
 
 	function index(){
 		$this->inbox();
+		$testing = "what";
 	}
 
 	function inbox($sort_by = 'sent_date', $sort_order = 'desc', $offset = 0){
