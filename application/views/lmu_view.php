357fc1ae52bed0d7b823ceb7b0f78aef81d792d9
Aaron Mast <amast09@gmail.com>
2014-03-16 15:29:57 -0400

    Fixed inadvertent id dependency bug.

--- application/views/lmu_view.php
+++ application/views/lmu_view.php
@@ -27,7 +27,7 @@
 		</div>
 	</div>
 
-	<a class="pull-left back-button" href="<?=site_url('world')?>"  data-site-url="<?=site_url()?>">
+	<a class="pull-left back-button" href="<?=site_url('world')?>" id="site" data-site-url="<?=site_url()?>">
 		<i class="icon icon-arrow-left"></i>World Map
 	</a>
 </div>
