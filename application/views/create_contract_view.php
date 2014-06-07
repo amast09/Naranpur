aa01761aa25e105096dc2a8865b5e89c021f2a8d
Aaron Mast <amast09@gmail.com>
2014-06-07 14:50:37 -0400

    Finished CSS & JS for the navigation between steps of the creation of a contract.
    Also changed arrangement of the steps and the navigation between them.

--- application/views/create_contract_view.php
+++ application/views/create_contract_view.php
@@ -1,55 +1,89 @@
-<div class="container-fluid">
-	<div class="row-fluid">
-		<h1>Contract Options</h1>
-	</div>
-	<div class="row-fluid">
-		<div class='span4 family-names'>
-			<ul class='family-name-list'>
+<!-- Steps of Contract Creation -->
+<div class="wrap step-1">
+
+	<!-- Employee Choosing Step -->
+	<div class="container-fluid employee-option">
+		<div class="row-fluid">
+			<h1>Choose the Employee</h1>
+		</div>
+		<div class="row-fluid">
+			<div class='span4 family-names'>
+				<ul class='family-name-list'>
+					<li class="header">Family</li>
+					<?php
+						foreach($families->result() as $family) {
+							echo "<li class='family-name' data-family-name='$family->name'>$family->name</li>";
+						}
+					?>
+				</ul>
+			</div>
+
+			<div class='span8 family-members'>
 				<?php
-					foreach($families->result() as $family) {
-						echo "<li class='family-name' data-family-name='$family->name'>$family->name</li>";
+					$last_family = "";
+					$member_index = 0;
+					for($i = 0; $i < $members->num_rows(); $i++) {
+						$member = $members->row($i);
+						$member_index++;
+						$start_of_family = $last_family != $member->family_name;
+						$end_of_family = ($i == $members->num_rows() - 1 || $member->family_name != $members->row($i + 1)->family_name);
+
+						if($start_of_family){
+							echo "<ul class='family-member-list' id='family-$member->family_name'>" .
+										"<li class='header'>" .
+											"<ul class='family-member-stats inline'>" .
+												"<li class='span3'>Member</li>" .
+												"<li class='span2'>Age</li>" .
+												"<li class='span2'>Sex</li>" .
+												"<li class='span2'>Health</li>" .
+												"<li class='span3'>Labor</li>" .
+											"</ul>" .
+										"</li>";
+						}
+
+						echo	"<li class='family-member'>" .
+										"<ul class='family-member-stats inline'>" .
+											"<li class='name span3'>member-$member_index</li>" .
+											"<li class='age span2'>$member->age</li>" .
+											"<li class='sex span2'>$member->sex</li>" .
+											"<li class='health span2'>$member->health</li>" .
+											"<li class='labor span3'>120</li>" .
+										"</ul>" .
+									"</li>";
+
+						if($end_of_family) {
+							echo "</ul>";
+							$member_index = 0;
+						}
+
+						$last_family = $member->family_name;
 					}
 				?>
-			</ul>
-		</div>
-
-		<div class='span8 family-members'>
-			<?php
-				$last_family = "";
-				$member_index = 0;
-				for($i = 0; $i < $members->num_rows(); $i++) {
-					$member = $members->row($i);
-					$member_index++;
-					$start_of_family = $last_family != $member->family_name;
-					$end_of_family = ($i == $members->num_rows() - 1 || $member->family_name != $members->row($i + 1)->family_name);
-
-					if($start_of_family){
-						echo "<ul class='family-member-list' id='family-$member->family_name'>" .
-									"<li>" .
-										"<div class='span3'>Member</div>" .
-										"<div class='span3'>Age</div>" .
-										"<div class='span3'>Sex</div>" .
-										"<div class='span3'>Health</div>" .
-									"</li>";
-					}
-
-					echo	"<li class='family-member'>" .
-									"<ul class='family-member-stats inline'>" .
-										"<li class='name span3'>member-$member_index</li>" .
-										"<li class='age span3'>$member->age</li>" .
-										"<li class='sex span3'>$member->sex</li>" .
-										"<li class='health span3'>$member->health</li>" .
-									"</ul>" .
-								"</li>";
-
-					if($end_of_family) {
-						echo "</ul>";
-						$member_index = 0;
-					}
-
-					$last_family = $member->family_name;
-				}
-			?>
+			</div>
 		</div>
 	</div>
+
+	<!-- Resource Choosing Step -->
+	<div class="container-fluid resources-option">
+		<div class="row-fluid">
+			<h1>Choose the Resources</h1>
+		</div>
+	</div>
+
+	<!-- Contract Review Step -->
+	<div class="container-fluid review-option">
+		<div class="row-fluid">
+			<h1>Review the Contract</h1>
+		</div>
+	</div>
+
 </div>
+
+<!-- Navigation of Contract Creation Next/Previous Buttons -->
+<div class="container-fluid navigation">
+	<div class="row-fluid">
+		<button class="btn btn-primary pull-left previous" type="button">Previous</button>
+		<button class="btn btn-primary pull-right next" type="button">Next</button>
+	</div>
+</div>
+
--- public_html/resources/create_contract_view/js/createContract.js
+++ public_html/resources/create_contract_view/js/createContract.js
@@ -1,32 +1,74 @@
 $(function() {
 
+	// If you click a family
   $(".family-name").on("click", function(){
+		// Grab the DOM node with the associated family members
 		var	$familyToShow = $("#family-" + $(this).attr("data-family-name"));
-
+		// Make sure the next button is hidden
+		hideNextButton();
+		// Make sure to unselect any family member previously selected
+		$(".family-member").removeClass("chosen");
+		// If the family was already selected
 		if($familyToShow.is(':visible')) {
-			$(this).removeClass("chosen");
+			// Hide the family members
 			$familyToShow.hide();
+			// Unselect the family
+			$(this).removeClass("chosen");
 		} else {
+			// Unselect any other family selection
+			$(".family-name").removeClass('chosen');
+			// Hide any previous family members
 			$(".family-member-list").hide();
-			$(".family-name").removeClass("chosen");
+			// Select that family
 			$(this).addClass("chosen");
+			// Show the family members of that associated family
 			$familyToShow.show();
 		}
 
   });
 
+// If you click a family member
 	$(".family-member").on("click", function(){
-		var $memberToShow = $("#member-" + $(this).attr("data-member-id"));
-
-		if($memberToShow.is(':visible')) {
+		// If the family member was already selected
+		if($(this).hasClass('chosen')) {
+			// Remove the hightlight as well as the next button
 			$(this).removeClass("chosen");
-			$memberToShow.hide();
+			hideNextButton();
 		} else {
+			// Remove any previous family member selection
 			$(".family-member").removeClass("chosen");
+			// Select that family member
 			$(this).addClass("chosen");
-			$memberToShow.show();
+			// Make sure the next button is showing
+			showNextButton();
 		}
 
   });
+
+  $(".next").on("click", function(){
+		var $wrap = $(".wrap");
+		if($wrap.hasClass('step-1')) {
+			$wrap.removeClass('step-1').addClass('step-2');
+		} else if($wrap.hasClass('step-2')) {
+			$wrap.removeClass('step-2').addClass('step-3');
+		}
+  });
+
+  $(".previous").on("click", function(){
+		var $wrap = $(".wrap");
+		if($wrap.hasClass('step-2')) {
+			$wrap.removeClass('step-2').addClass('step-1');
+		} else if($wrap.hasClass('step-3')) {
+			$wrap.removeClass('step-3').addClass('step-2');
+		}
+  });
+
+  function hideNextButton() {
+		$(".wrap").removeClass("employee-chosen");
+  }
+
+  function showNextButton() {
+		$(".wrap").addClass("employee-chosen");
+  }
 
 });
